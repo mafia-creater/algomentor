d0d6c9c9f0c28a3ba229470184713a837278e018
Ripudaman Singh <78353770+mafia-creater@users.noreply.github.com>
2025-12-04 15:22:06 +0530

    Change Redis connection URL to use rediss

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,6 +1,6 @@
 import Redis from "ioredis";
 
-const client = new Redis("redis://default:ATVtAAIncDI0ZDRkNzdiNzY1M2Q0NTM5YmNjMWE1YTYzOTRlMmEzM3AyMTM2Nzc@advanced-calf-13677.upstash.io:6379");
+const client = new Redis("rediss://default:ATVtAAIncDI0ZDRkNzdiNzY1M2Q0NTM5YmNjMWE1YTYzOTRlMmEzM3AyMTM2Nzc@advanced-calf-13677.upstash.io:6379");
 
 // Optionally, you can add a test connection function for debugging:
 // export async function testRedisConnection() {

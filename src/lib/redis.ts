cd27c1fbbeda41bf6225709d707dcad53e2ef586
Ripudaman Singh <78353770+mafia-creater@users.noreply.github.com>
2025-12-04 15:14:19 +0530

    Update Redis connection string

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,6 +1,6 @@
 import Redis from "ioredis";
 
-const client = new Redis("rediss://default:ARO-AAIjcDExZGUzODY2M2FkNDY0M2VlYTY4YzMxNmE1ZTFhZjVlN3AxMA@sterling-cub-5054.upstash.io:6379");
+const client = new Redis("redis://default:ATVtAAIncDI0ZDRkNzdiNzY1M2Q0NTM5YmNjMWE1YTYzOTRlMmEzM3AyMTM2Nzc@advanced-calf-13677.upstash.io:6379");
 
 // Optionally, you can add a test connection function for debugging:
 // export async function testRedisConnection() {
